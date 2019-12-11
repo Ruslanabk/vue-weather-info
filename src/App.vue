cec0767298c63497c8f52eed9e5a2cae4db91a4d
Ruslan Abkadirov <rusabk@yandex.ru>
2019-12-12 02:52:20 +0300

    [edit] main app component constructed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InformerContainer>
+      <InformerTemperature temp-real="10" temp-feel="7"></InformerTemperature>
+      <FlexBreak></FlexBreak>
+      <InformerHumidity>12</InformerHumidity>
+      <VerticalDivider></VerticalDivider>
+      <InformerWind>13</InformerWind>
+    </InformerContainer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InformerContainer from './components/InformerContainer.vue'
+import InformerTemperature from './components/InformerTemperature.vue'
+import InformerHumidity from './components/InformerHumidity.vue'
+import InformerWind from './components/InformerWind.vue'
+import VerticalDivider from './components/VerticalDivider.vue'
+import FlexBreak from './components/FlexBreak.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    InformerContainer,
+    InformerTemperature,
+    InformerHumidity,
+    InformerWind,
+    VerticalDivider,
+    FlexBreak
+  },
+  
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
