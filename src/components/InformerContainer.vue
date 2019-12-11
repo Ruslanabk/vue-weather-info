cd2d412ce6a4b0d512b45ec3008fc15e6fcd51da
Ruslan Abkadirov <rusabk@yandex.ru>
2019-12-12 02:12:14 +0300

    [create] components filed was created]

---
+++ src/components/InformerContainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="informer-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InformerContainer"
+};
+</script>
+
+<style>
+.informer-container {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 400px;
+  padding: 30px;
+  background-image: url("../assets/img/forest.png");
+  background-size: cover;
+  background-position: center center;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.75);
+}
+</style>
