17e2bec61ed5aeb9551b8f7bfd82aa6aab0e0b73
Ruslan Abkadirov <rusabk@yandex.ru>
2019-12-12 14:25:39 +0300

    auto update interval added

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,8 @@ export default {
   },
   data() {
     return {
-      weather: {}
+      weather: {},
+      timerWeatherUpd: ""
     };
   },
   methods: {
@@ -47,6 +48,7 @@ export default {
   },
   mounted() {
     this.updWeather();
+    this.timerWeatherUpd = setInterval(this.updWeather, 1000);
   }
 };
 </script>
